ec15b9c35d460c3c630a4782a51678321e69846b
bogoO1 <brandonhando01@gmail.com>
2023-08-18 15:05:08 -0700

    Finished

---
+++ README.md
@@ -0,0 +1,1 @@
+Project 1 in the following YouTube video. https://www.youtube.com/watch?v=qwfE7fSVaZM
--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-console.log("Task Manager App");
+require("./db/connect");
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+require("dotenv").config();
+//middleware
+app.use(express.json());
+app.use(express.static("./public"));
+//routes
 
-// routes
+app.use("/api/v1/tasks", tasks);
 
-app.get('/hello', (req, res) => {
-	res.send('Task Manager App')
-})
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+start();
